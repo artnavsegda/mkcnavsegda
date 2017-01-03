87bccd9b612369547ce0c1ab1b8b7ebd1cb8d79d
Art Navsegda <artnavsegda@gmail.com>
2017-01-03 19:14:38 +0300

    ssaka

--- xmega/unit-test/blink/blink.c
+++ xmega/unit-test/blink/blink.c
@@ -3,6 +3,11 @@ sfr sbit LED1 at PORTR_OUT.B1;
 sfr sbit LED2 at PORTD_OUT.B4;
 sfr sbit LED3 at PORTD_OUT.B5;
 
+sfr sbit LED0_Direction at PORTA_DIR.B0;
+sfr sbit LED1_Direction at PORTA_DIR.B1;
+sfr sbit LED2_Direction at PORTD_DIR.B4;
+sfr sbit LED3_Direction at PORTD_DIR.B5;
+
 void main() {
 
 }
