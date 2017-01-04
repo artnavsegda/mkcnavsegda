aa0ed241e15d851592cb7a2ecdc7179bea0dedc8
Art Navsegda <artnavsegda@gmail.com>
2017-01-05 02:11:03 +0300

    wake me ep

--- xmega/unit-test/blink/xmega_a3bu_xplained.c
+++ xmega/unit-test/blink/xmega_a3bu_xplained.c
@@ -8,6 +8,8 @@ sfr sbit LED1_Direction at PORTR_DIR.B1;
 sfr sbit LED2_Direction at PORTD_DIR.B4;
 sfr sbit LED3_Direction at PORTD_DIR.B5;
 
+sfr sbit LCD_BACKLIGHT_ENABLE_PIN at PORTE_OUT.B4;
+
 sfr sbit GPIO_PUSH_BUTTON_0 at PORTE_OUT.B5;
 sfr sbit GPIO_PUSH_BUTTON_1 at PORTF_OUT.B1;
 sfr sbit GPIO_PUSH_BUTTON_2 at PORTF_OUT.B2;
@@ -20,6 +22,12 @@ sfr sbit SPIC_SS at PORTC_OUT.B4;
 sfr sbit SPIC_MOSI at PORTC_OUT.B5;
 sfr sbit SPIC_MISO at PORTC_OUT.B6;
 sfr sbit SPIC_SCK at PORTC_OUT.B7;
+
+sfr sbit USARTD0_RXD at PORTD_OUT.B2;
+sfr sbit USARTD0_TXD at PORTD_OUT.B3;
+
+sfr sbit USARTE0_RXD at PORTE_OUT.B2;
+sfr sbit USARTE0_TXD at PORTE_OUT.B3;
 
 sfr sbit J1_PIN0 at PORTC_OUT.B0;
 sfr sbit J1_PIN1 at PORTC_OUT.B1;
@@ -63,5 +71,24 @@ void board_init(void)
         LED1_Direction = 1;
         LED2_Direction = 1;
         LED3_Direction = 1;
+
+        PORTE_PIN5CTRL = 0x18; // gpio push button 0 pull up
+        PORTF_PIN1CTRL = 0x18; // gpio push button 1 pull up
+        PORTF_PIN1CTRL = 0x18; // gpio push button 2 pull up
         
-        
+#ifdef CONF_BOARD_ENABLE_USARTC0
+	PORTC_DIR.B3 = 1;
+	PORTC_OUT.B2 = 0;
+#endif
+
+#ifdef CONF_BOARD_ENABLE_USARTD0
+	PORTD_DIR.B3 = 1;
+	PORTD_OUT.B2 = 0;
+#endif
+
+#ifdef CONF_BOARD_ENABLE_USARTE0
+	PORTE_DIR.B3 = 1;
+	PORTE_OUT.B2 = 0;
+#endif
+
+}
