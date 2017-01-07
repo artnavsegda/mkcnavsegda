8fc0ab925b725dab1638420e93c31639f50b95a9
Art Navsegda <artnavsegda@gmail.com>
2017-01-08 02:36:43 +0300

    i2c

--- xmega/unit-test/i2c/i2c.c
+++ xmega/unit-test/i2c/i2c.c
@@ -1,3 +1,36 @@
-void main() {
+void TWIE_Send(unsigned char devaddr, unsigned char regaddr, unsigned char value)
+{
+        TWIE_Start();
+        TWIE_Write(devaddr << 1);
+        TWIE_Write(regaddr);
+        TWIE_Write(value);
+        TWIE_Stop();
+}
 
+unsigned char TWIE_Recieve(unsigned char devaddr, unsigned char regaddr)
+{
+	unsigned char value;
+        TWIE_Start();
+        TWIE_Write(devaddr << 1);
+        TWIE_Write(regaddr);
+        TWIE_Start();
+        TWIE_Write((devaddr<<1)+1);
+        value = TWIE_Read(0);
+        TWIE_Stop();
+        return value;
+}
+
+void main()
+{
+        TWIE_Init(100000);
+        TWIE_Send(0x1a,0x03,0x00);
+        TWIE_Send(0x1a,0x02,0x00);
+
+        while (1)
+        {
+                Delay_100ms();
+                TWIE_Send(0x1a,0x01,0x00);
+                Delay_100ms();
+                TWIE_Send(0x1a,0x01,0xFF);
+        }
 }
