c049ba7d4d894a716ae50ca90fd98ac7220d99ba
Art Navsegda <artnavsegda@gmail.com>
2017-01-08 09:33:43 +0300

    disregard the constabulary

--- xmega/unit-test/ethernet/ethernet.c
+++ xmega/unit-test/ethernet/ethernet.c
@@ -1,15 +1,15 @@
 #include  <__EthEnc28j60.h>
 
-sfr sbit SPI_Ethernet_Rst at PORTC_OUT.B0;
-sfr sbit SPI_Ethernet_CS  at PORTC_OUT.B1;
-sfr sbit SPI_Ethernet_Rst_Direction at PORTC_DIR.B0;
-sfr sbit SPI_Ethernet_CS_Direction  at PORTC_DIR.B1;
+sfr sbit SPI_Ethernet_Rst at PORTA_OUT.B0;
+sfr sbit SPI_Ethernet_CS  at PORTC_OUT.B0;
+sfr sbit SPI_Ethernet_Rst_Direction at PORTA_DIR.B0;
+sfr sbit SPI_Ethernet_CS_Direction  at PORTC_DIR.B0;
 
 unsigned char   myMacAddr[6] = {0x00, 0x14, 0xA5, 0x76, 0x19, 0x3f} ;   // my MAC address
-unsigned char   myIpAddr[4]  = {192, 168, 20, 60} ;                     // my IP address
-unsigned char   gwIpAddr[4]  = {192, 168,  20,  6 } ;                   // gateway (router) IP address
+unsigned char   myIpAddr[4]  = {192, 168, 1, 150} ;                     // my IP address
+unsigned char   gwIpAddr[4]  = {192, 168,  1,  1 } ;                   // gateway (router) IP address
 unsigned char   ipMask[4]    = {255, 255, 255,  0 } ;                   // network mask (for example : 255.255.255.0)
-unsigned char   dnsIpAddr[4] = {192, 168,  20,  1 } ;                   // DNS server IP address
+unsigned char   dnsIpAddr[4] = {192, 168,  1,  1 } ;                   // DNS server IP address
 
 unsigned int SPI_Ethernet_UserTCP(unsigned char *remoteHost, unsigned int remotePort, unsigned int localPort, unsigned int reqLength, TEthPktFlags *flags)
 {
