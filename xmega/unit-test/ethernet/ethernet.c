e0949763aa79fa34c90172566b20b84bbc77aa53
Art Navsegda <artnavsegda@gmail.com>
2017-01-09 00:23:41 +0300

    kekius

--- xmega/unit-test/ethernet/ethernet.c
+++ xmega/unit-test/ethernet/ethernet.c
@@ -5,12 +5,6 @@ sfr sbit SPI_Ethernet_CS  at PORTC_OUT.B0;
 sfr sbit SPI_Ethernet_Rst_Direction at PORTA_DIR.B0;
 sfr sbit SPI_Ethernet_CS_Direction  at PORTC_DIR.B0;
 
-unsigned char   myMacAddr[6] = {0x00, 0x14, 0xA5, 0x76, 0x19, 0x3f} ;   // my MAC address
-unsigned char   myIpAddr[4]  = {192, 168, 1, 150} ;                     // my IP address
-unsigned char   gwIpAddr[4]  = {192, 168,  1,  1 } ;                   // gateway (router) IP address
-unsigned char   ipMask[4]    = {255, 255, 255,  0 } ;                   // network mask (for example : 255.255.255.0)
-unsigned char   dnsIpAddr[4] = {192, 168,  1,  1 } ;                   // DNS server IP address
-
 unsigned int SPI_Ethernet_UserTCP(unsigned char *remoteHost, unsigned int remotePort, unsigned int localPort, unsigned int reqLength, TEthPktFlags *flags)
 {
         return 0;
@@ -24,8 +18,8 @@ unsigned int SPI_Ethernet_UserUDP(unsigned char *remoteHost, unsigned int remote
 void main() {
         SPIC_Init();
         PORTC_OUT.B4 = 1; //important to disable SPIC SS prior to configure ethernet
-        SPI_Ethernet_Init(myMacAddr, myIpAddr, 0x01);
-        SPI_Ethernet_confNetwork(ipMask, gwIpAddr, dnsIpAddr);
+        SPI_Ethernet_Init("\x00\x14\xA5\x76\x19\x3f", "\xC0\xA8\x01\x96", 0x01);
+        SPI_Ethernet_confNetwork("\xFF\xFF\xFF\x00", "\xC0\xA8\x01\x01", "\xC0\xA8\x01\x01");
         
         while(1)
                 SPI_Ethernet_doPacket();
