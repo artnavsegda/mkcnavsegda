4d657b21ff7f3c334b52dd76d8322b4683bed83b
Art Navsegda <artnavsegda@gmail.com>
2017-01-08 07:32:41 +0300

    kek

--- xmega/unit-test/i2c/i2c.c
+++ xmega/unit-test/i2c/i2c.c
@@ -1,36 +1,59 @@
-void TWIE_Send(unsigned char devaddr, unsigned char regaddr, unsigned char value)
+//unsigned char FUBAR;
+//sbit FUBAR_first at FUBAR.B0;
+
+void Expander_Write_Byte(char ModuleAddress, char RegAddress, char Data_)
 {
         TWIE_Start();
-        TWIE_Write(devaddr << 1);
-        TWIE_Write(regaddr);
-        TWIE_Write(value);
+        TWIE_Write(ModuleAddress << 1);
+        TWIE_Write(RegAddress);
+        TWIE_Write(Data_);
         TWIE_Stop();
 }
 
-unsigned char TWIE_Recieve(unsigned char devaddr, unsigned char regaddr)
+char Expander_Read_Byte(char ModuleAddress, char RegAddress)
 {
-	unsigned char value;
+        unsigned char value;
         TWIE_Start();
-        TWIE_Write(devaddr << 1);
-        TWIE_Write(regaddr);
+        TWIE_Write(ModuleAddress << 1);
+        TWIE_Write(RegAddress);
         TWIE_Start();
-        TWIE_Write((devaddr<<1)+1);
+        TWIE_Write((ModuleAddress<<1)+1);
         value = TWIE_Read(0);
         TWIE_Stop();
         return value;
 }
 
-void main()
+void Expander_Set_DirectionPort(char ModuleAddress, char Data_)
+{
+        Expander_Write_Byte(ModuleAddress, 0x03, Data_);
+}
+
+void Expander_Write_Port(char ModuleAddress, char Data_)
+{
+        Expander_Write_Byte(ModuleAddress, 0x01, Data_);
+}
+
+char Expander_Read_Port(char ModuleAddress)
+{
+	return Expander_Read_Byte(ModuleAddress, 0x00);
+}
+
+void Expander_Init(char ModuleAddress)
 {
         TWIE_Init(100000);
-        TWIE_Send(0x1a,0x03,0x00);
-        TWIE_Send(0x1a,0x02,0x00);
+        Expander_Set_DirectionPort(ModuleAddress,0x00);
+        Expander_Write_Byte(ModuleAddress,0x02,0x00);
+}
+
+void main()
+{
+        Expander_Init(0x1a);
 
         while (1)
         {
                 Delay_100ms();
-                TWIE_Send(0x1a,0x01,0x00);
+                Expander_Write_Port(0x1a,0x00);
                 Delay_100ms();
-                TWIE_Send(0x1a,0x01,0xFF);
+                Expander_Write_Port(0x1a,0xFF);
         }
 }
