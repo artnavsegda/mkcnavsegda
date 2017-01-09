c75a2a86b896807b03831c72e9db053a945545fa
Art Navsegda <artnavsegda@gmail.com>
2017-01-09 07:08:38 +0300

    ethernet

---
+++ xmega/prototype/ethernet.c
@@ -0,0 +1,11 @@
+#include  <__EthEnc28j60.h>
+
+unsigned int SPI_Ethernet_UserTCP(unsigned char *remoteHost, unsigned int remotePort, unsigned int localPort, unsigned int reqLength, TEthPktFlags *flags)
+{
+        return 0;
+}
+
+unsigned int SPI_Ethernet_UserUDP(unsigned char *remoteHost, unsigned int remotePort, unsigned int localPort, unsigned int reqLength, TEthPktFlags *flags)
+{
+        return 0;
+}
