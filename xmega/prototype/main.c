7bdd172484b241a1d9512d4c742ce7b2c814e669
Art Navsegda <artnavsegda@gmail.com>
2017-01-09 09:16:16 +0300

    kekanus

--- xmega/prototype/main.c
+++ xmega/prototype/main.c
@@ -5,12 +5,27 @@ void PrintHandler(char c)
 
 void Sysclk_Init(void)
 {
-	OSC_CTRL = 0x02;
-	while(RC32MRDY_bit == 0);
-	CPU_CCP = 0xD8;
-	CLK_CTRL = 1;
+        OSC_CTRL = 0x02;
+        while(RC32MRDY_bit == 0);
+        CPU_CCP = 0xD8;
+        CLK_CTRL = 1;
+}
+
+void Prototype_Init(void)
+{
+        Sysclk_Init();
+        UARTC0_Init(115200);
+        UART_Set_Active(&UARTC0_Read, &UARTC0_Write, &UARTC0_Data_Ready, &UARTC0_Tx_Idle);
+        Timer_Init(&TCC0, 1000000);
+        Timer_Interrupt_Enable(&TCC0);
+        SPIC_Init();
+        SPI_Ethernet_Init("\x00\x14\xA5\x76\x19\x3f", "\xC0\xA8\x01\x96", 0x01);
+        SPI_Ethernet_confNetwork("\xFF\xFF\xFF\x00", "\xC0\xA8\x01\x01", "\xC0\xA8\x01\x01");
 }
 
 void main() {
-
+	Prototype_Init();
+	
+	while(1)
+	        SPI_Ethernet_doPacket();
 }
