ff54086bab078a25fa05866ea86e4d1b1277f6d9
Art Navsegda <artnavsegda@gmail.com>
2017-01-10 09:06:29 +0300

    doctor

--- xmega/modbus/main.c
+++ xmega/modbus/main.c
@@ -2,17 +2,32 @@
 #include "ad7705.h"
 #include "ports.h"
 #include "bswap.h"
+#include "modbus.h"
 
 void PrintHandler(char c)
 {
-        UARTC0_Write(c);
+        UART_Write(c);
+}
+
+int tick = 0;
+
+void Timer0Overflow_ISR() org IVT_ADDR_TCC0_OVF
+{
+        tick = 1;
 }
 
 void main()
 {
+	int i;
         unsigned int result;
         LED0_Direction = 1;
+        LED2_Direction = 1;
         UARTC0_Init(115200);
+        UART_Set_Active(&UARTC0_Read, &UARTC0_Write, &UARTC0_Data_Ready, &UARTC0_Tx_Idle);
+        Timer_Init(&TCC0, 1000000);
+        Timer_Interrupt_Enable(&TCC0);
+        PMIC_CTRL = 4;
+        CPU_SREG.B7 = 1;
         AD7705_Init();
         SPI_Ethernet_Init("\x00\x14\xA5\x76\x19\x3f", "\xC0\xA8\x01\x96", 0x01);
         SPI_Ethernet_confNetwork("\xFF\xFF\xFF\x00", "\xC0\xA8\x01\x01", "\xC0\xA8\x01\x01");
@@ -24,6 +39,17 @@ void main()
                 {
                         LED0_Toggle = 1;
                         AD7705_Read_Register(0x38,(unsigned char *)&result,2);
+                }
+                if (tick == 1)
+                {
+			LED2_Toggle = 1;
+			table[2]++;
+			table[3] = BSWAP_16(result);
+			for (i = 0; i<8;i++)
+                                table[4+i] = ADCA_Read(i);
+                        for (i = 0; i<8;i++)
+                                table[12+i] = ADCB_Read(i);
+                        tick = 0;
                 }
         }
 }
