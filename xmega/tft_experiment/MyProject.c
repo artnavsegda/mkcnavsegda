72502bf80a22fc4c5cc26a1c5c761d4fc01dc6f1
Art Navsegda <artnavsegda@gmail.com>
2017-01-20 22:35:25 +0300

    tft

---
+++ xmega/tft_experiment/MyProject.c
@@ -0,0 +1,49 @@
+// TFT display connections
+char TFT_16bit_DataPort_Lo at PORTC_OUT;
+char TFT_16bit_DataPort_Hi at PORTA_OUT;
+sbit TFT_16bit_WR at PORTE_OUT.B3;
+sbit TFT_16bit_RD at PORTE_OUT.B2;
+sbit TFT_16bit_CS at PORTE_OUT.B1;
+sbit TFT_16bit_RS at PORTE_OUT.B0;
+sbit TFT_16bit_RST at PORTD_OUT.B0;
+
+char TFT_16bit_DataPort_Lo_Direction at PORTC_DIR;
+char TFT_16bit_DataPort_Hi_Direction at PORTA_DIR;
+sbit TFT_16bit_WR_Direction at PORTE_DIR.B3;
+sbit TFT_16bit_RD_Direction at PORTE_DIR.B2;
+sbit TFT_16bit_CS_Direction at PORTE_DIR.B1;
+sbit TFT_16bit_RS_Direction at PORTE_DIR.B0;
+sbit TFT_16bit_RST_Direction at PORTD_DIR.B0;
+// End of TFT display connections
+
+
+void Sysclk_Init(void)
+{
+        OSC_CTRL = 0x02;
+        while(RC32MRDY_bit == 0);
+        CPU_CCP = 0xD8;
+        CLK_CTRL = 1;
+}
+
+void main() {
+        TFT_16bit_Rotate_180(1);
+        TFT_Init_ILI9341_16bit(240, 320);
+        
+        TFT_16bit_Fill_Screen(CL_BLACK_16bit);
+        //delay_ms(1000);
+        
+        TFT_16bit_Set_Font(TFT_16bit_defaultFont, CL_WHITE_16bit, FO_VERTICAL_16bit);
+
+        TFT_16bit_Write_Text("TFT 16-bit Library DEMO, WELCOME !", 0, 319);
+        
+        while(1)
+        {
+                //TFT_16bit_Fill_Screen(CL_LIME_16bit);
+                //delay_ms(1000);
+                //TFT_16bit_Fill_Screen(CL_WHITE_16bit);
+                //delay_ms(1000);
+                
+                //TFT_16bit_Write_Text("TFT 16-bit Library DEMO, WELCOME !", 0, 0);
+                
+        }
+}
