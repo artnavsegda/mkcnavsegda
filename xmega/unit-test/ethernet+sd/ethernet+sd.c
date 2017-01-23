069896eb1ad5be90bf8a6dd379ce8318b1ecb5da
Art Navsegda <artnavsegda@gmail.com>
2017-01-23 04:29:58 +0300

    dumalka

--- xmega/unit-test/ethernet+sd/ethernet+sd.c
+++ xmega/unit-test/ethernet+sd/ethernet+sd.c
@@ -1,3 +1,6 @@
 void main() {
-
+	UARTC0_Init(115200);
+        Delay_ms(10);
+        UARTC0_Write_Text("MCU-Started\r\n");
+        SPIC_Init_Advanced(_SPI_MASTER, _SPI_FCY_DIV2, _SPI_CLK_LO_LEADING);
 }
