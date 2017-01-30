fea4240639075115855d9c1e041d994ab5072d16
Art Navsegda <artnavsegda@gmail.com>
2017-01-30 06:12:02 +0300

    ethernet

--- pic16f46k20/ethernet/ethernet.c
+++ pic16f46k20/ethernet/ethernet.c
@@ -1,21 +1,61 @@
 #include  <__EthEnc28j60.h>
+#include "httpUtils.h"
+
+#define HTTP_REQUEST_SIZE       128
+
+#define putConstString  SPI_Ethernet_putConstString
 
 sfr sbit SPI_Ethernet_Rst at RD2_bit;
 sfr sbit SPI_Ethernet_CS  at RD3_bit;
 sfr sbit SPI_Ethernet_Rst_Direction at TRISD2_bit;
 sfr sbit SPI_Ethernet_CS_Direction  at TRISD3_bit;
 
+const unsigned char httpHeader[] = "HTTP/1.1 200 OK\nContent-type: " ;  // HTTP header
+const unsigned char httpMimeTypeHTML[] = "text/html\n\n" ;              // HTML MIME type
+const unsigned char httpMimeTypeScript[] = "text/plain\n\n" ;           // TEXT MIME type
+
+/*
+ * HTTP + HTML common header
+ */
+const char    *HTMLheader = "\
+<HTML><HEAD>\
+<TITLE>MikroElektronika</TITLE>\
+</HEAD><BODY>\
+<h2>MikroElektronika</h2>\
+" ;
+
+/*
+ * HTML common footer
+ */
+const   char    *HTMLfooter = "\
+</BODY></HTML>\n\n" ;
+
 unsigned char   myMacAddr[6] = {0x00, 0x14, 0xA5, 0x76, 0x19, 0x3f} ;   // my MAC address
 unsigned char   myIpAddr[4]  = {192, 168, 1, 150} ;                     // my IP address
 
 unsigned int  SPI_Ethernet_UserTCP(unsigned char *remoteHost, unsigned int remotePort, unsigned int localPort, unsigned int reqLength, TEthPktFlags *flags)
 {
-	return 0;
+        unsigned int    len = 0;            // my reply length
+	unsigned char   getRequest[HTTP_REQUEST_SIZE + 1];
+        if(localPort != 80)                     // I listen only to web request on port 80
+        {
+                return(0);                     // return without reply
+        }
+        if(HTTP_getRequest(getRequest, &reqLength, HTTP_REQUEST_SIZE) == 0)
+        {
+                return(0);                     // no reply if no GET request
+        }
+        len = putConstString(httpHeader);              // HTTP header
+        len += putConstString(httpMimeTypeHTML);     // with text MIME type
+        len += putConstString(HTMLheader);
+        len += putConstString("Hello, world !");
+        len += putConstString(HTMLfooter);
+        return len;
 }
 
 unsigned int  SPI_Ethernet_UserUDP(unsigned char *remoteHost, unsigned int remotePort, unsigned int destPort, unsigned int reqLength, TEthPktFlags *flags)
 {
-	return 0;
+        return 0;
 }
 
 void main()
