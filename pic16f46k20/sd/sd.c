6da10a10f9ebba729f8d981debbf8ad74255f77d
Art Navsegda <artnavsegda@gmail.com>
2017-01-30 19:10:55 +0300

    update

--- pic16f46k20/sd/sd.c
+++ pic16f46k20/sd/sd.c
@@ -1,3 +1,29 @@
-void main() {
+sbit Mmc_Chip_Select           at LATD3_bit;  // for writing to output pin always use latch (PIC18 family)
+sbit Mmc_Chip_Select_Direction at TRISD3_bit;
 
+void main()
+{
+	char sd_init, sd_format, sd_exists, sd_assign;
+        UART1_Init(115200);
+        Delay_ms(10);
+        UART1_Write_Text("MCU-Started\r\n");
+        SPI1_Init_Advanced(_SPI_MASTER_OSC_DIV64, _SPI_DATA_SAMPLE_MIDDLE, _SPI_CLK_IDLE_LOW, _SPI_LOW_2_HIGH);
+        
+        sd_init = Mmc_Fat_Init();
+        
+        switch(sd_init)
+        {
+                case 0:
+                        UART1_Write_Text("SD FAT Init-OK\r\n");
+                break;
+                case 1:
+                        UART1_Write_Text("SD FAT not found\r\n");
+                break;
+                default:
+                        UART1_Write_Text("SD Init-error\r\n");
+                break;
+        }
+        
+        while (1)
+                ;
 }
