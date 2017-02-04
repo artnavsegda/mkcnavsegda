4859e315a6f50e5f66899c5a4097337ae4a2c933
Art Navsegda <artnavsegda@gmail.com>
2017-02-04 07:44:57 +0300

    this

--- xmega/modbus/wwwroot/ipmac.js
+++ xmega/modbus/wwwroot/ipmac.js
@@ -5,8 +5,5 @@ function fillform()
         xhttp.open("LOL", "getopt", false);
         xhttp.send("ip");
         document.forms["settings"]["ip"].value = xhttp.responseText;
-        xhttp.open("LOL", "getopt", false);
-        xhttp.send("mac");
-        document.forms["settings"]["mac"].value = xhttp.responseText;
         return false;
 }
