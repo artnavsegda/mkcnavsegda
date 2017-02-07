bf8aba7b60d2d25ea34c6018231aad7a992f9c48
Art Navsegda <artnavsegda@gmail.com>
2017-02-08 00:34:46 +0300

    blink

---
+++ mikroxmega/blink/blink.c
@@ -0,0 +1,18 @@
+void main() {
+	PORTA_DIR = 0xFF;
+	PORTB_DIR = 0xFF;
+	PORTC_DIR = 0xFF;
+	PORTA_OUT = 0xFF;
+	PORTB_OUT = 0xFF;
+	PORTC_OUT = 0xFF;
+
+        while (1)
+        {
+		delay_ms(300);
+		PORTA_OUTTGL = 0xFF;
+		delay_ms(300);
+		PORTB_OUTTGL = 0xFF;
+		delay_ms(300);
+		PORTC_OUTTGL = 0xFF;
+	}
+}
