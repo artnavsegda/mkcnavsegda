68fe5e4b9222936537a15632c2fab3c8bc901ed0
Art Navsegda <artnavsegda@gmail.com>
2017-02-08 18:55:43 +0300

    ext8

--- mikroxmega/sysclk/ext8/sysclkext8.c
+++ mikroxmega/sysclk/ext8/sysclkext8.c
@@ -1,3 +1,13 @@
 void main() {
+// ----- External Clock selection procedure!!!
+   OSC_XOSCCTRL = 0x40;   // freq. range 2 - 9MHz; External Clock
+   OSC_CTRL |= 0x08;      // enable external oscillator
 
+   while(XOSCRDY_bit == 0)
+    ;
+
+   CPU_CCP = 0xD8;  // The CCP register must be written with the correct signature to enable change of the protected
+                    // I/O register or execution of the protected instruction for a maximum of 4 CPU instruction cycles.
+   CLK_CTRL = 3;    // External oscillator
+// ---------------------------------
 }
