bf76a280478d113349ac2c0cf17ad33d019d50c8
Art Navsegda <artnavsegda@gmail.com>
2017-02-09 20:24:55 +0300

    heap

--- xmega/modbus/ethernet.c
+++ xmega/modbus/ethernet.c
@@ -1,16 +1,13 @@
 #include  <__EthEnc28j60.h>
 #include "modbus.h"
-#include "httpUtils.h"
 #include "global.h"
 #include "http.h"
 #include "tftp.h"
 
-#define HTTP_REQUEST_SIZE       1410
-
 unsigned int SPI_Ethernet_UserTCP(unsigned char *remoteHost, unsigned int remotePort, unsigned int localPort, unsigned int reqLength, TEthPktFlags *flags)
 {
         static struct mbframestruct askframe;
-        static unsigned char getRequest[HTTP_REQUEST_SIZE + 1];
+        unsigned char *getRequest;
         unsigned char *method;
         unsigned char *page;
         unsigned char *buf2;
@@ -19,16 +16,15 @@ unsigned int SPI_Ethernet_UserTCP(unsigned char *remoteHost, unsigned int remote
         {
                 case 80:
                 {
+                        getRequest = Malloc(reqLength+1);
                         SPI_Ethernet_getBytes(getRequest, 0xFFFF, reqLength);
                         getRequest[reqLength] = 0;
                         method = strtok(getRequest," ");
                         page = strtok(0," ");
-                        //buf2 = strtok(0,0);
                         buf2 = page+strlen(page)+1;//ugly but well fuck
-                        //if(HTTP_getRequest(getRequest, &reqLength, HTTP_REQUEST_SIZE) == 0)
-                        //        return 0;
                         flags->canCloseTCP = 1;
                         len = http(page,buf2);
+                        Free(getRequest,reqLength+1);
                         return len;
                 }
                 break;
