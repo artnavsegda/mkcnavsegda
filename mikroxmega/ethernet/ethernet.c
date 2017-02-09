29fe151b0c15900a458e139a59b275ce33df8cdc
Art Navsegda <artnavsegda@gmail.com>
2017-02-09 04:32:06 +0300

    ethernet

--- mikroxmega/ethernet/ethernet.c
+++ mikroxmega/ethernet/ethernet.c
@@ -1,3 +1,26 @@
-void main() {
+#include  <__EthEnc28j60.h>
 
+sfr sbit SPI_Ethernet_Rst at PORTC_OUT.B1;
+sfr sbit SPI_Ethernet_CS  at PORTC_OUT.B0;
+sfr sbit SPI_Ethernet_Rst_Direction at PORTC_DIR.B1;
+sfr sbit SPI_Ethernet_CS_Direction  at PORTC_DIR.B0;
+
+unsigned int SPI_Ethernet_UserTCP(unsigned char *remoteHost, unsigned int remotePort, unsigned int localPort, unsigned int reqLength, TEthPktFlags *flags)
+{
+        return 0;
+}
+
+unsigned int SPI_Ethernet_UserUDP(unsigned char *remoteHost, unsigned int remotePort, unsigned int localPort, unsigned int reqLength, TEthPktFlags *flags)
+{
+        return 0;
+}
+
+void main() {
+        SPIC_Init();
+        //PORTC_OUT.B4 = 1; //important to disable SPIC SS prior to configure ethernet
+        SPI_Ethernet_Init("\x00\x14\xA5\x76\x19\x3f", "\xC0\xA8\x01\x96", 0x01);
+        SPI_Ethernet_confNetwork("\xFF\xFF\xFF\x00", "\xC0\xA8\x01\x01", "\xC0\xA8\x01\x01");
+
+        while(1)
+                SPI_Ethernet_doPacket();
 }
