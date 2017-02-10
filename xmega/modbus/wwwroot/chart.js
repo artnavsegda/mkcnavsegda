bc1461192c01885afb2a4aa8fdcc9a226a4429e5
Art Navsegda <artnavsegda@gmail.com>
2017-02-11 00:03:33 +0300

    zoom

--- xmega/modbus/wwwroot/chart.js
+++ xmega/modbus/wwwroot/chart.js
@@ -18,7 +18,7 @@ function dropdata2()
 
 function createTimeline()
 {
-        var smoothie = new SmoothieChart({millisPerPixel:100,maxValue:7000,minValue:5000,grid:{millisPerLine:5000,verticalSections:10,strokeStyle:'#202020',sharpLines:true},interpolation:'linear'});
+        var smoothie = new SmoothieChart({millisPerPixel:100,maxValue:6500,minValue:5500,grid:{millisPerLine:5000,verticalSections:10,strokeStyle:'#202020',sharpLines:true},interpolation:'linear'});
         smoothie.addTimeSeries(line2,{strokeStyle:'rgb(200,0,0)'});
         smoothie.addTimeSeries(line1,{lineWidth:2});
         smoothie.streamTo(document.getElementById("mycanvas"));
