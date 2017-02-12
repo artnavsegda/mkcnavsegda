ee69a30216e88d2319f2a14fab05b21841f9cef5
Art Navsegda <artnavsegda@gmail.com>
2017-02-12 04:04:09 +0300

    blink

--- mikroxmega/blink/blink.c
+++ mikroxmega/blink/blink.c
@@ -1,18 +1,28 @@
-void main() {
-	PORTA_DIR = 0xFF;
-	PORTB_DIR = 0xFF;
-	PORTC_DIR = 0xFF;
-	PORTA_OUT = 0xFF;
-	PORTB_OUT = 0xFF;
-	PORTC_OUT = 0xFF;
+void main()
+{
+	// ----- External Clock selection procedure!!!
+	OSC_XOSCCTRL = 0x40;   // freq. range 2 - 9MHz; External Clock
+	OSC_CTRL |= 0x08;      // enable external oscillator
+	while(XOSCRDY_bit == 0);
+	CPU_CCP = 0xD8;  // The CCP register must be written with the correct signature to enable change of the protected
+			 // I/O register or execution of the protected instruction for a maximum of 4 CPU instruction cycles.
+	CLK_CTRL = 3;    // External oscillator
+	// ---------------------------------
+
+        PORTA_DIR = 0xFF;
+        PORTB_DIR = 0xFF;
+        PORTC_DIR = 0xFF;
+        PORTA_OUT = 0xFF;
+        PORTB_OUT = 0xFF;
+        PORTC_OUT = 0xFF;
 
         while (1)
         {
-		delay_ms(300);
-		PORTA_OUTTGL = 0xFF;
-		delay_ms(300);
-		PORTB_OUTTGL = 0xFF;
-		delay_ms(300);
-		PORTC_OUTTGL = 0xFF;
-	}
+                delay_ms(300);
+                PORTA_OUTTGL = 0xFF;
+                delay_ms(300);
+                PORTB_OUTTGL = 0xFF;
+                delay_ms(300);
+                PORTC_OUTTGL = 0xFF;
+        }
 }
