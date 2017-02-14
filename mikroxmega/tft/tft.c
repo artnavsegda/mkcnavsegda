83da963e4ea60c76bf06427edda86d927647f6e0
Art Navsegda <artnavsegda@gmail.com>
2017-02-14 14:50:32 +0300

    tft

--- mikroxmega/tft/tft.c
+++ mikroxmega/tft/tft.c
@@ -1,10 +1,33 @@
+/*
+ * Project name:
+     UART (Simple usage of UART module library functions)
+ * Copyright:
+     (c) Mikroelektronika, 2010.
+ * Revision History:
+     20080930:
+       - initial release;
+ * Description:
+     This code demonstrates how to use UART library routines. Upon receiving
+     data via RS232, MCU immediately sends it back to the sender.
+ * Test configuration:
+     MCU:             ATxmega128A1
+                      http://www.atmel.com/dyn/resources/prod_documents/doc8067.pdf
+     Dev.Board:       -
+     Oscillator:      Internal Clock 2.0000 MHz
+     Ext. Modules:    -
+     SW:              mikroC PRO for AVR
+                      http://www.mikroe.com/eng/products/view/228/mikroc-pro-for-avr/
+ * NOTES:
+     -
+ */
+
 // TFT display connections
 char TFT_16bit_DataPort_Lo at PORTJ_OUT;
 char TFT_16bit_DataPort_Hi at PORTK_OUT;
 sbit TFT_16bit_WR at PORTH_OUT.B5;
 sbit TFT_16bit_RD at PORTH_OUT.B6;
-sbit TFT_16bit_CS at PORTB_OUT.B3;
-sbit TFT_16bit_RS at PORTB_OUT.B4;
+sbit TFT_16bit_CS at PORTH_OUT.B3;
+sbit TFT_16bit_RS at PORTH_OUT.B4;
 sbit TFT_16bit_RST at PORTH_OUT.B7;
 
 char TFT_16bit_DataPort_Lo_Direction at PORTJ_DIR;
@@ -16,38 +39,40 @@ sbit TFT_16bit_RS_Direction at PORTH_DIR.B4;
 sbit TFT_16bit_RST_Direction at PORTH_DIR.B7;
 // End of TFT display connections
 
-
-void SysClk_Init() {
-        // ----- External Clock selection procedure!!!
-        OSC_XOSCCTRL = 0x40;   // freq. range 2 - 9MHz; External Clock
-        OSC_CTRL |= 0x08;      // enable external oscillator
-        while(XOSCRDY_bit == 0);
-        CPU_CCP = 0xD8;  // The CCP register must be written with the correct signature to enable change of the protected
-                         // I/O register or execution of the protected instruction for a maximum of 4 CPU instruction cycles.
-        CLK_CTRL = 3;    // External oscillator
-        // ---------------------------------
-}
-
+char uart_rd;
 void main() {
-	SysClk_Init();
-        TFT_16bit_Rotate_180(1);
-        TFT_Init_ILI9341_16bit(240, 320);
 
-        TFT_16bit_Fill_Screen(CL_BLACK_16bit);
-        //delay_ms(1000);
+// ----- External Clock selection procedure!!!
+   OSC_XOSCCTRL = 0x40;   // freq. range 2 - 9MHz; External Clock
+   OSC_CTRL |= 0x08;      // enable external oscillator
 
-        TFT_16bit_Set_Font(TFT_16bit_defaultFont, CL_WHITE_16bit, FO_VERTICAL_16bit);
+   while(XOSCRDY_bit == 0)
+    ;
 
-        TFT_16bit_Write_Text("TFT 16-bit Library DEMO, WELCOME !", 0, 319);
+   CPU_CCP = 0xD8;  // The CCP register must be written with the correct signature to enable change of the protected
+                    // I/O register or execution of the protected instruction for a maximum of 4 CPU instruction cycles.
+   CLK_CTRL = 3;    // External oscillator
+// ---------------------------------
 
-        while(1)
-        {
-                TFT_16bit_Fill_Screen(CL_LIME_16bit);
-                delay_ms(1000);
-                TFT_16bit_Fill_Screen(CL_WHITE_16bit);
-                delay_ms(1000);
+  UARTC0_Init(19200);              // Initialize UART module at 19200 bps
+  Delay_ms(1000);                  // Wait for UART module to stabilize
 
-                TFT_16bit_Write_Text("TFT 16-bit Library DEMO, WELCOME !", 0, 0);
+  UARTC0_Write_Text("Start");
 
-        }
+
+// Initialize 240x320 TFT display
+TFT_Init_ILI9341_16bit(240, 320);
+
+TFT_16bit_Fill_Screen(CL_LIME_16bit);
+
+
+  while (1) {                      // Endless loop
+  TFT_16bit_Fill_Screen(CL_TEAL_16bit);
+  TFT_16bit_Fill_Screen(CL_MAROON_16bit);
+   if (UARTC0_Data_Ready()) {      // If data is received,
+     uart_rd = UARTC0_Read();      // read the received data,
+     UARTC0_Write(uart_rd);        // and send data via UART
+    }
+  }
+
 }
