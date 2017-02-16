6c8e210e0b08c4ae9dd0b91f88ada6ab7a938458
Art Navsegda <artnavsegda@gmail.com>
2017-02-16 23:18:30 +0300

    inverse

--- xmega/modbus/main.c
+++ xmega/modbus/main.c
@@ -47,13 +47,13 @@ void Fill_Table(void)
 {
         char lowlight = ADC_Voltage(ADCB_Get_Sample(ADCB_PMT_Voltage)) < 1.0;
         char lowflow = ADC_Voltage(ADCB_Get_Sample(ADCB_Flow)) < 1.0;
-        bctable[0] = lowlight | lowflow;
-        bctable[1] = PORTU3_IN.B6 | PORTU2_IN.B7 | PORTU1_IN.B1 | PORTU2_IN.B3 | PORTU2_IN.B4;
+        bctable[0] = !(lowlight | lowflow);
+        bctable[1] = !(PORTU3_IN.B6 | PORTU2_IN.B7 | PORTU1_IN.B1 | PORTU2_IN.B3 | PORTU2_IN.B4);
         splitfloat(&table[8],&table[9], (float)currentmode);
         if (currentmode == TOTALMERCURY)
-                splitfloat(&table[10],&table[11], (((float)(zerostage-zerolevelavg)/(float)(celllevelavg-zerolevelavg))*(1297.17*exp(0.0082*(TMP_Celsius(ADC_Voltage(celltempavg))-25)))));
+                splitfloat(&table[10],&table[11], (((float)(zerostage-zerolevelavg)/(float)(celllevelavg-zerolevelavg))*(1297.17*exp(0.082*(TMP_Celsius(ADC_Voltage(celltempavg))-25)))));
         else
-                splitfloat(&table[12],&table[13], (((float)(zerostage-zerolevelavg)/(float)(celllevelavg-zerolevelavg))*(1297.17*exp(0.0082*(TMP_Celsius(ADC_Voltage(celltempavg))-25)))));
+                splitfloat(&table[12],&table[13], (((float)(zerostage-zerolevelavg)/(float)(celllevelavg-zerolevelavg))*(1297.17*exp(0.082*(TMP_Celsius(ADC_Voltage(celltempavg))-25)))));
         splitfloat(&table[14],&table[15], ADC_Voltage(ADCB_Get_Sample(ADCB_Flow)));
         splitfloat(&table[16],&table[17], ADC_Voltage(ADCA_Get_Sample(ADCA_Vacuum)));
         splitfloat(&table[18],&table[19], ADC_Voltage(ADCA_Get_Sample(ADCA_Dilution)));
