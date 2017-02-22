490c3ab55eeb7c7eef56b6967a79feb686a2cf34
Art Navsegda <artnavsegda@gmail.com>
2017-02-22 18:13:12 +0300

    kkk

--- xmega/modbus/wwwroot/formula.js
+++ xmega/modbus/wwwroot/formula.js
@@ -7,14 +7,23 @@ function dropdata()
         xhttp.open("GET", "getdata?zerolevelavg", false);
         xhttp.send();
         var zerolevelavg = xhttp.responseText;
+        document.getElementById("zerolevelavg1").innerHTML = zerolevelavg;
+        document.getElementById("zerolevelavg2").innerHTML = zerolevelavg;
         xhttp.open("GET", "getdata?celllevelavg", false);
         xhttp.send();
         var celllevelavg = xhttp.responseText;
+        document.getElementById("celllevelavg").innerHTML = celllevelavg;
         xhttp.open("GET", "getdata?celltempavg", false);
         xhttp.send();
         var celltempavg = (((xhttp.responseText-186)*((3.283/1.603)/4095))-0.5)*100;
-        xhttp.open("GET", "getdata?x16", false);
+        document.getElementById("celltempavg").innerHTML = celltempavg.toFixed(2);
+        xhttp.open("GET", "getsample", false);
         xhttp.send();
-        var demo = ((xhttp.responseText-zerolevelavg)/(celllevelavg-zerolevelavg))*(1297.17*Math.exp(0.082*(celltempavg-25)));
+        var arava = JSON.parse(xhttp.responseText);
+        var ololo = 0;
+        for (i = 0; i < 64; i++){ololo += arava[i]}
+        var kekus = ololo/64;
+        document.getElementById("kekus").innerHTML = kekus.toFixed(2);
+        var demo = (((ololo/64)-zerolevelavg)/(celllevelavg-zerolevelavg))*(1297.17*Math.exp(0.082*(celltempavg-25)));
         document.getElementById("demo").innerHTML = demo.toFixed(6);
 }
