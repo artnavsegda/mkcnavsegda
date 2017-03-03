cae496386030a56f5996c12363d5e26b27246396
Art Navsegda <artnavsegda@gmail.com>
2017-03-03 04:09:28 +0300

    vbat

---
+++ xmega/unit-test/rtc/rtc.c
@@ -0,0 +1,19 @@
+unsigned long rtc_get_time(void)
+{
+        RTC32_SYNCCTRL = SYNCCNT;
+        while (SYNCCNT_bit & SYNCCNT)
+        return RTC32_CNT;
+}
+
+void rtc_set_time(unsigned long time)
+{
+        RTC32_CTRL = 0;
+        while (SYNCBUSY_RTC32_SYNCCTRL_bit)
+        RTC32_CNT = time;
+        ENABLE_RTC32_CTRL_bit = 1;
+}
+
+void main()
+{
+
+}
