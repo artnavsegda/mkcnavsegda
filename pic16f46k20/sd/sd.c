29ffd690ee0ce4f1a835ce51c67f835c6ecf31dc
Art Navsegda <artnavsegda@gmail.com>
2017-03-26 02:20:25 +0300

    subculture

--- pic16f46k20/sd/sd.c
+++ pic16f46k20/sd/sd.c
@@ -1,16 +1,13 @@
-sbit Mmc_Chip_Select           at LATB0_bit;  // for writing to output pin always use latch (PIC18 family)
-sbit Mmc_Chip_Select_Direction at TRISB0_bit;
+sbit Mmc_Chip_Select           at LATD3_bit;  // for writing to output pin always use latch (PIC18 family)
+sbit Mmc_Chip_Select_Direction at TRISD3_bit;
 
 void main()
 {
         char sd_init, sd_format, sd_exists, sd_assign;
-        UART1_Init(115200);
+        UART1_Init(9600);
         Delay_ms(10);
         UART1_Write_Text("MCU-Started\r\n");
-        TRISD3_bit = 0; //set ethernet cs as output
         SPI1_Init_Advanced(_SPI_MASTER_OSC_DIV64, _SPI_DATA_SAMPLE_MIDDLE, _SPI_CLK_IDLE_LOW, _SPI_LOW_2_HIGH);
-        LATD3_bit = 1; //shut up ethernet controller
-        
         sd_init = Mmc_Fat_Init();
         
         switch(sd_init)
@@ -24,6 +21,23 @@ void main()
                 default:
                         UART1_Write_Text("SD Init-error\r\n");
                 break;
+        }
+        
+        if (sd_init == 1)
+        {
+                sd_format = Mmc_Fat_QuickFormat("DIGITAL");
+                switch(sd_format)
+                {
+                        case 0:
+                                UART1_Write_Text("Card was detected, successfully formated and initialized\r\n");
+                        break;
+                        case 1:
+                                UART1_Write_Text("FAT16 format was unsuccessful\r\n");
+                        break;
+                        default:
+                                UART1_Write_Text("MMC/SD card was not detected\r\n");
+                        break;
+                }
         }
         
         while (1)
