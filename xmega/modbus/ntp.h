d4e96977ee7313ba1ca047288f9f65f75b372e10
Art Navsegda <artnavsegda@gmail.com>
2017-04-11 14:33:35 +0300

    rtc+ntp

---
+++ xmega/modbus/ntp.h
@@ -0,0 +1,20 @@
+#define NTP_TIME_OFFSET 2208988800
+
+struct ntptime {
+        unsigned long timeseconds;
+        unsigned long timefraction;
+};
+
+struct ntpframestruct {
+        unsigned char leapvermode;
+        unsigned char stratumlevel;
+        unsigned char poll;
+        unsigned char precision;
+        unsigned long rootdelay;
+        unsigned long driftrate;
+        unsigned long referenceid;
+        struct ntptime reference;
+        struct ntptime origin;
+        struct ntptime receive;
+        struct ntptime transmit;
+};
