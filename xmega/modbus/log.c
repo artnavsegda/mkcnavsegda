9c44384eea3bdeb60bf034a810701b1daa506b7e
Art Navsegda <artnavsegda@gmail.com>
2017-05-10 15:27:01 +0300

    log

--- xmega/modbus/log.c
+++ xmega/modbus/log.c
@@ -1,12 +1,26 @@
 #include "global.h"
+#include "timelib.h"
+#include "rtc.h"
 #include <__Lib_MmcFat16.h>
 
 short logfile;
 
+void LogHandler(char c)
+{
+        Mmc_Fat_Write(&c, 1);
+        //UART_Write(c);
+}
+
 void Log_Write(char message[])
 {
+	static TimeStruct ts2;
         Mmc_Fat_Activate(logfile);
+        Time_epochToDate(rtc_get_time(), &ts2);
+        PrintOut(LogHandler, "%d.%d.%d %d:%d:%d: ", ts2.yy,ts2.mo,ts2.md,ts2.hh,ts2.mn,ts2.ss);
         Mmc_Fat_Write(message, strlen(message));
+        //UART_Write_Text(message);
+        Mmc_Fat_Write("\r\n", 2);
+        //UART_Write_Text("\r\n");
 }
 
 void Log_Init(void)
@@ -15,5 +29,5 @@ void Log_Init(void)
         {
                 logfile = Mmc_Fat_Open("LOG.TXT",FILE_APPEND,0x80);
                 Log_Write("Device started");
-	}
+        }
 }
