47b13c6871770ea7180fc30f742d2874f213ef8a
Art Navsegda <artnavsegda@gmail.com>
2017-05-23 02:46:53 +0300

    gecks

--- xmega/unit-test/spi/spi.c
+++ xmega/unit-test/spi/spi.c
@@ -1,5 +1,7 @@
 #define BSWAP_16(x) ((((x) >> 8) & 0xff) | (((x) & 0xff) << 8))
-sfr sbit LED0 at PORTR_OUT.B0;
+
+sfr sbit AD7705_CS at PORTC_OUT.B4;
+sfr sbit AD7705_DRDY at PORTC_IN.B1;
 sfr sbit LED0_Direction at PORTR_DIR.B0;
 sfr sbit LED0_Toggle at PORTR_OUTTGL.B0;
 
@@ -10,25 +12,11 @@ void SPI_Read_Bytes(char *buffer, unsigned NoBytes)
                 buffer[i] = SPI_Read(0xFF);
 }
 
-void SPIC_Read_Bytes(char *buffer, unsigned NoBytes)
-{
-        int i;
-        for (i = 0; i < NoBytes; i++)
-                buffer[i] = SPIC_Read(0xFF);
-}
-
 void SPI_Write_Bytes(char *buffer, unsigned NoBytes)
 {
         int i;
         for (i = 0; i < NoBytes; i++)
                 SPI_Write(buffer[i]);
-}
-
-void SPIC_Write_Bytes(char *buffer, unsigned NoBytes)
-{
-        int i;
-        for (i = 0; i < NoBytes; i++)
-                SPIC_Write(buffer[i]);
 }
 
 void PrintHandler(char c)
@@ -38,28 +26,30 @@ void PrintHandler(char c)
 
 void main() {
         unsigned int result;
-        LED0_Direction = 1;//led0
         PORTC_DIR.B4 = 1;
-        PORTCFG_CLKEVOUT.CLKEVPIN = 1;
-        PORTCFG_CLKEVOUT.CLKOUT0 = 1;
+        PORTC_OUT.B4 = 0;
         UARTC0_Init(9600);
-        SPIC_Init_Advanced(_SPI_MASTER, _SPI_FCY_DIV8, _SPI_CLK_LO_LEADING);
+        SPIC_Init_Advanced(_SPI_MASTER, _SPI_FCY_DIV4, _SPI_CLK_LO_LEADING);
         SPI_Set_Active(&SPIC_Read, &SPIC_Write);
         SPI_Write_Bytes("\xFF\xFF\xFF\xFF\xFF", 5);
-        delay_ms(100);
-        SPI_Write_Bytes("\x20\x0C\x10\x04", 4);
-        delay_ms(100);
+        delay_ms(10);
+        SPI_Write_Bytes("\x20\x0C",2);
+        delay_ms(10);
+        SPI_Write_Bytes("\x10\x04", 2);
+        delay_ms(10);
         SPI_Write_Bytes("\x60\x18\x3A\x00", 4);
-        delay_ms(100);
+        delay_ms(10);
         SPI_Write_Bytes("\x70\x89\x78\xD7", 4);
-        delay_ms(100);
+        delay_ms(10);
 
         while (1)
         {
-		LED0_Toggle = 1;//led0
-		SPIC_Write(0x38);
-		SPIC_Read_Bytes((unsigned char *)&result,2);
-		PrintOut(PrintHandler, "%x\r\n", BSWAP_16(result));
-		delay_ms(100);
+                if (AD7705_DRDY == 0)
+                {
+                        LED0_Toggle = 1;
+                	SPI_Write(0x38);
+                	SPI_Read_Bytes((unsigned char *)&result,2);
+                        PrintOut(PrintHandler, "hex: %5x\r\n", BSWAP_16(result));
+                  }
         }
 }
