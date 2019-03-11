7804aa2eea30ac33f638e811857e3c36856690cc
Art Navsegda <artnavsegda@gmail.com>
2019-03-11 14:00:14 +0300

    serial modem update

--- stm32/STM32F101VG/serial/serial.c
+++ stm32/STM32F101VG/serial/serial.c
@@ -1,9 +1,9 @@
 void main() {
-     UART1_Init(9600);
+     UART4_Init(9600);
      Delay_ms(100);
      while(1)
      {
-             UART1_Write_Text("test\r\n");
+             UART4_Write_Text("test\r\n");
              Delay_ms(1000);
      }
 }
