aa83a084cf6744966b906e9c885dd2a95f794062
Art Navsegda <artnavsegda@gmail.com>
2019-03-15 15:57:46 +0300

    Update seriallink.c

--- stm32/STM32F101VG/seriallink/seriallink.c
+++ stm32/STM32F101VG/seriallink/seriallink.c
@@ -1,7 +1,25 @@
+long long num = 0;
+
+void Timer2_interrupt() iv IVT_INT_TIM2 {
+     TIM2_SR.UIF = 0;
+     num++;
+}
+
 void main() {
      char exetx, exerx;
      char string[10];
      int i = 0;
+     
+     RCC_APB1ENR.TIM2EN = 1;       // Enable clock gating for timer module 2
+     TIM2_CR1.CEN = 0;             // Disable timer
+     TIM2_PSC = 0;              // Set timer prescaler.
+     TIM2_ARR = 7999;
+     NVIC_IntEnable(IVT_INT_TIM2); // Enable timer interrupt
+     TIM2_DIER.UIE = 1;            // Update interrupt enable
+     TIM2_CR1.CEN = 1;             // Enable timer
+     
+     
+     
      UART1_Init(9600);//(RX+/-)
      UART3_Init(9600);//(TX+/-)
      UART4_Init(115200);//stdio
@@ -11,18 +29,14 @@ void main() {
              if (UART1_Data_Ready())
              {
                 exerx = UART1_Read();
-                sprintf(string,"RX %d\r\n",exerx);
+                sprintf(string,"RX1 %X %lu\r\n",exerx,num);
                 UART4_Write_Text(string);
              }
              if (UART3_Data_Ready())
              {
                 exetx = UART3_Read();
-                sprintf(string,"TX %d\r\n",exetx);
+                sprintf(string,"RX3 %X %lu\r\n",exetx,num);
                 UART4_Write_Text(string);
              }
-             //i++;
-             //sprintf(string,"test %d\r\n",i);
-             //UART4_Write_Text(string);
-             //Delay_ms(100);
      }
 }
