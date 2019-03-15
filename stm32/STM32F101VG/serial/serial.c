2382d3661505562b1e4040a5109c7b48a2f0a62c
Art Navsegda <artnavsegda@gmail.com>
2019-03-15 15:34:15 +0300

    serial timer

--- stm32/STM32F101VG/serial/serial.c
+++ stm32/STM32F101VG/serial/serial.c
@@ -1,13 +1,33 @@
+unsigned long long num = 0;
+
+void Timer2_interrupt() iv IVT_INT_TIM2 {
+     TIM2_SR.UIF = 0;
+     GPIOD_ODR = ~GPIOD_ODR;      // Toggle PORTE led's
+     num++;
+}
+
 void main() {
      char string[10];
      int i = 0;
+     GPIO_Digital_Output(&GPIOD_BASE, _GPIO_PINMASK_ALL);  // Enable digital output on PORTD
+     GPIOD_ODR = 0;
+
+     RCC_APB1ENR.TIM2EN = 1;       // Enable clock gating for timer module 2
+     TIM2_CR1.CEN = 0;             // Disable timer
+     TIM2_PSC = 0;              // Set timer prescaler.
+     TIM2_ARR = 7999;
+     NVIC_IntEnable(IVT_INT_TIM2); // Enable timer interrupt
+     TIM2_DIER.UIE = 1;            // Update interrupt enable
+     TIM2_CR1.CEN = 1;             // Enable timer
+
      UART4_Init(9600);
      Delay_ms(100);
+
      while(1)
      {
-             i++;
-             sprintf(string,"test %d\r\n",i);
-             UART4_Write_Text(string);
-             Delay_ms(100);
+      i++;
+      sprintf(string,"test %lu\r\n",num);
+      UART4_Write_Text(string);
+      Delay_ms(100);
      }
 }
