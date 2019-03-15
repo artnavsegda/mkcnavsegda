df34820ddfbbcaa5309f9861df239e310962e80d
Art Navsegda <artnavsegda@gmail.com>
2019-03-15 14:31:24 +0300

    Update timer.c

--- stm32/STM32F101VG/timer/timer/timer.c
+++ stm32/STM32F101VG/timer/timer/timer.c
@@ -1,11 +1,11 @@
 void Timer2_interrupt() iv IVT_INT_TIM2 {
   TIM2_SR.UIF = 0;
-  GPIOE_ODR = ~GPIOE_ODR;      // Toggle PORTE led's
+  GPIOD_ODR = ~GPIOD_ODR;      // Toggle PORTE led's
 }
 
 void main() {
-  GPIO_Digital_Output(&GPIOE_BASE, _GPIO_PINMASK_ALL);  // Enable digital output on PORTE
-  GPIOE_ODR = 0;
+  GPIO_Digital_Output(&GPIOD_BASE, _GPIO_PINMASK_ALL);  // Enable digital output on PORTD
+  GPIOD_ODR = 0;
 
   RCC_APB1ENR.TIM2EN = 1;       // Enable clock gating for timer module 2
   TIM2_CR1.CEN = 0;             // Disable timer
