4511321e35914a6e2174449fe716a05ae1d5dc85
Art Navsegda <artnavsegda@gmail.com>
2019-03-18 16:59:30 +0300

    slave

--- stm32/STM32F207ZG/exeslave/exeslave.c
+++ stm32/STM32F207ZG/exeslave/exeslave.c
@@ -1,3 +1,14 @@
 void main() {
-
+     char string[10];
+     UART1_Init(115200);//(stdio/aux3)
+     UART5_Init(9600);//mdb/exe2
+     UART1_Write_Text("hello123\r\n");
+     while(1)
+     {
+      if (UART5_Data_Ready())
+      {
+        sprintf(string,"RX5 %X\r\n",UART5_Read());
+        UART1_Write_Text(string);
+      }
+     }
 }
