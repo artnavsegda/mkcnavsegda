5345a518e101421fbaed06ae6187240ecc520761
Art Navsegda <artnavsegda@gmail.com>
2019-03-19 13:43:09 +0300

    boom

--- stm32/STM32F207ZG/i2cpcf/i2cpcf.c
+++ stm32/STM32F207ZG/i2cpcf/i2cpcf.c
@@ -3,20 +3,27 @@
 // I2C_SDA << I2C3_SDA << PC9
 //
 
-PCF_WrSingle(unsigned short wAddr, unsigned short wData)
+PCF_WrSingle(unsigned char wAddr, unsigned char wData)
 {
+     unsigned char buf[1];
+     buf[0] = wData;
      I2C3_Start();
-     I2C3_Write(wAddr,wData,1,END_MODE_STOP);
+     I2C3_Write(wAddr,buf,1,END_MODE_STOP);
 }
 
 void main() {
-     I2C3_Init();
+     //I2C3_Init();
+     I2C3_Init_Advanced(100000, &_GPIO_MODULE_I2C3_PA8_C9);
      
      while(1)
      {
-      PCF_WrSingle(0x00,0x00);
-      Delay_ms(100);
-      PCF_WrSingle(0x00,0xFF);
-      Delay_ms(100);
+      PCF_WrSingle(0x3C,0x00);
+      PCF_WrSingle(0x3D,0x00);
+      PCF_WrSingle(0x3E,0x00);
+      Delay_ms(1000);
+      PCF_WrSingle(0x3C,0xFF);
+      PCF_WrSingle(0x3D,0xFF);
+      PCF_WrSingle(0x3E,0xFF);
+      Delay_ms(1000);
      }
 }
