7315d6bedc5c64bd603ecbb73be0a227319a5e15
Art Navsegda <artnavsegda@gmail.com>
2019-03-19 12:41:59 +0300

    Update i2cpcf.c

--- stm32/STM32F207ZG/i2cpcf/i2cpcf.c
+++ stm32/STM32F207ZG/i2cpcf/i2cpcf.c
@@ -3,6 +3,20 @@
 // I2C_SDA << I2C3_SDA << PC9
 //
 
+PCF_WrSingle(unsigned short wAddr, unsigned short wData)
+{
+     I2C3_Start();
+     I2C3_Write(wAddr,wData,1,END_MODE_STOP);
+}
+
 void main() {
      I2C3_Init();
+     
+     while(1)
+     {
+      PCF_WrSingle(0x00,0x00);
+      Delay_ms(100);
+      PCF_WrSingle(0x00,0xFF);
+      Delay_ms(100);
+     }
 }
