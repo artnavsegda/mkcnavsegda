0de2e31d7bef63210b50c4c0e4cced85c9b75df6
Art Navsegda <artnavsegda@gmail.com>
2019-03-19 15:03:12 +0300

    rabotaet

--- stm32/STM32F207ZG/exeslave/exeslave.c
+++ stm32/STM32F207ZG/exeslave/exeslave.c
@@ -1,14 +1,24 @@
+void PrintHandler(char c) {
+     UART1_Write(c);
+}
+
+PCF_WrSingle(unsigned char wAddr, unsigned char wData)
+{
+     unsigned char buf[1];
+     buf[0] = wData;
+     I2C3_Start();
+     I2C3_Write(wAddr,buf,1,END_MODE_STOP);
+}
+
 void main() {
-     char string[10];
+     I2C3_Init_Advanced(100000, &_GPIO_MODULE_I2C3_PA8_C9);
      UART1_Init(115200);//(stdio/aux3)
-     UART5_Init(9600);//mdb/exe2
      UART1_Write_Text("hello123\r\n");
+     UART5_Init(9600);//mdb/exe2
+     PCF_WrSingle(0x20, 0b00000000); //mdb/exe2_m/s
      while(1)
      {
       if (UART5_Data_Ready())
-      {
-        sprintf(string,"RX5 %X\r\n",UART5_Read());
-        UART1_Write_Text(string);
-      }
+        PrintOut(PrintHandler,"RX5 %X\r\n",UART5_Read());
      }
 }
