b713c4e96a2985321e93f9ae78720722cb12d821
Art Navsegda <artnavsegda@gmail.com>
2019-03-20 15:25:47 +0300

    Update exeslave.c

--- stm32/STM32F207ZG/exeslave/exeslave.c
+++ stm32/STM32F207ZG/exeslave/exeslave.c
@@ -15,7 +15,7 @@ void main() {
      UART1_Init(115200);//(stdio/aux3)
      UART1_Write_Text("hello123\r\n");
      UART5_Init_Advanced(9600, _UART_9_BIT_DATA, _UART_NOPARITY, _UART_ONE_STOPBIT, &_GPIO_MODULE_UART5_PC12_PD2);//mdb/exe2
-     UART4_CR1bits.M = 1; //9 bit data transfer
+     UART5_CR1bits.M = 1; //9 bit data transfer
      PCF_WrSingle(0x20, 0b00000000); //both ports slaves
      while(1)
      {
