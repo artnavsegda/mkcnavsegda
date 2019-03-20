4f77cec575d8628f2985b722f3bba0f8efbef838
Art Navsegda <artnavsegda@gmail.com>
2019-03-20 15:16:45 +0300

    Update exeslave.c

--- stm32/STM32F207ZG/exeslave/exeslave.c
+++ stm32/STM32F207ZG/exeslave/exeslave.c
@@ -14,12 +14,14 @@ void main() {
      I2C3_Init_Advanced(100000, &_GPIO_MODULE_I2C3_PA8_C9);
      UART1_Init(115200);//(stdio/aux3)
      UART1_Write_Text("hello123\r\n");
-     UART5_Init_Advanced(9600, _UART_8_BIT_DATA, _UART_EVENPARITY, _UART_ONE_STOPBIT, &_GPIO_MODULE_UART5_PC12_PD2);//mdb/exe2
+     UART5_Init_Advanced(9600, _UART_9_BIT_DATA, _UART_NOPARITY, _UART_ONE_STOPBIT, &_GPIO_MODULE_UART5_PC12_PD2);//mdb/exe2
+     UART4_CR1bits.M = 1; //9 bit data transfer
      UART5_CR1bits.PCE = 1; // parity control enable
-     PCF_WrSingle(0x20, 0b00000000); //mdb/exe2_m/s
+     PCF_WrSingle(0x20, 0b00000000); //both ports slaves
      while(1)
      {
       if (UART5_Data_Ready())
         PrintOut(PrintHandler,"RX5 %X\r\n",UART5_Read());
      }
 }
+
