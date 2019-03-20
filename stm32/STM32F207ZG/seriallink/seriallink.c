7be3256776ddf514404a6f4f8d87da5e703d517b
Art Navsegda <artnavsegda@gmail.com>
2019-03-20 10:19:28 +0300

    Update seriallink.c

--- stm32/STM32F207ZG/seriallink/seriallink.c
+++ stm32/STM32F207ZG/seriallink/seriallink.c
@@ -47,8 +47,10 @@ void main() {
 
      I2C3_Init_Advanced(100000, &_GPIO_MODULE_I2C3_PA8_C9);
      
-     //PCF_WrSingle(0x20, 0b00010000); //mdb/exe1_m/s
-     //PCF_WrSingle(0x20, 0b01000000); //mdb/exe2_m/s
+     PCF_WrSingle(0x20, 0b00010000); // first(exe1_m/s) port master second(mdb/exe2_m/s) port slave
+     //PCF_WrSingle(0x20, 0b01000000); //first(exe1_m/s) port slave second(mdb/exe2_m/s) port master
+     //PCF_WrSingle(0x20, 0b00000000); //both ports slaves
+     //PCF_WrSingle(0x20, 0b01010000); //both ports masters
 
      UART1_Init(115200);//(stdio/aux3)
 
@@ -60,7 +62,6 @@ void main() {
      UART5_CR1bits.RXNEIE = 1; // enable uart rx interrupt
      NVIC_IntEnable(IVT_INT_UART5); // enable interrupt vector
      
-     //UART5_Init_Advanced(9600, _UART_8_BIT_DATA, _UART_NOPARITY, _UART_ONE_STOPBIT, &_GPIO_MODULE_UART5_PC12_PD2); // this should work
      Delay_ms(100);
      UART1_Write_Text("hello123\r\n");
      while(1)
@@ -75,14 +76,14 @@ void main() {
       
       Delay_ms(100);
       
-      PrintOut(PrintHandler,"TX5 31 %lu\r\n",num);
+/*PrintOut(PrintHandler,"TX5 31 %lu\r\n",num);
       UART5_Write(0x31);
 
       Delay_ms(100);
 
       PrintOut(PrintHandler,"TX5 32 %lu\r\n",num);
       UART5_Write(0x32);
-      
-      Delay_ms(100);
+
+      Delay_ms(100);*/
      }
 }
