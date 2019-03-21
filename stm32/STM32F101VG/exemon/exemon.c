c5b2c306ca40e5b9486e4b6764586e3a532cd72e
Art Navsegda <artnavsegda@gmail.com>
2019-03-21 11:35:47 +0300

    exe mon

--- stm32/STM32F101VG/exemon/exemon.c
+++ stm32/STM32F101VG/exemon/exemon.c
@@ -25,7 +25,7 @@ void main() {
      char exetx, exerx;
      char string[10];
      int i = 0;
-     
+
      RCC_APB1ENR.TIM2EN = 1;       // Enable clock gating for timer module 2
      TIM2_CR1.CEN = 0;             // Disable timer
      TIM2_PSC = 0;              // Set timer prescaler.
@@ -33,17 +33,17 @@ void main() {
      NVIC_IntEnable(IVT_INT_TIM2); // Enable timer interrupt
      TIM2_DIER.UIE = 1;            // Update interrupt enable
      TIM2_CR1.CEN = 1;             // Enable timer
-     
-     UART1_Init_Advanced(9600, _UART_8_BIT_DATA, _UART_EVENPARITY, _UART_ONE_STOPBIT, &_GPIO_MODULE_USART1_PA9_10);    //(RX+/-)
-     
+
+     UART1_Init_Advanced(9600, _UART_9_BIT_DATA, _UART_NOPARITY, _UART_ONE_STOPBIT, &_GPIO_MODULE_USART1_PA9_10);    //(RX+/-)
+     USART1_CR1bits.M = 1; //9 bit data transfer
      USART1_CR1bits.RXNEIE = 1; // enable uart rx interrupt
      NVIC_IntEnable(IVT_INT_USART1); // enable interrupt vector
-     
-     UART3_Init_Advanced(9600, _UART_8_BIT_DATA, _UART_EVENPARITY, _UART_ONE_STOPBIT, &_GPIO_MODULE_USART3_PB10_11);    //(TX+/-)
-     
+
+     UART3_Init_Advanced(9600, _UART_9_BIT_DATA, _UART_NOPARITY, _UART_ONE_STOPBIT, &_GPIO_MODULE_USART3_PB10_11);    //(TX+/-)
+     USART3_CR1bits.M = 1; //9 bit data transfer
      USART3_CR1bits.RXNEIE = 1; // enable uart rx interrupt
      NVIC_IntEnable(IVT_INT_USART3); // enable interrupt vector
-     
+
      UART4_Init(115200);//stdio
      Delay_ms(100);
      UART4_Write_Text("starting\r\n");
