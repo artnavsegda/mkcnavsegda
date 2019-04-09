055507f9260e8afc212e68122f192d9be235c863
Art Navsegda <artnavsegda@gmail.com>
2019-04-09 16:04:50 +0300

    bq24295

---
+++ stm32/STM32F207ZG/bq24295/bq24295.c
@@ -0,0 +1,37 @@
+//
+// I2C_SCL << I2C3_SCL << PA8
+// I2C_SDA << I2C3_SDA << PC9
+//
+
+PCF_WrSingle(unsigned char wAddr, unsigned char wData)
+{
+     unsigned char buf[1];
+     buf[0] = wData;
+     I2C3_Start();
+     I2C3_Write(wAddr,buf,1,END_MODE_STOP);
+}
+
+unsigned char PCF_RdSingle(unsigned char wAddr)
+{
+      unsigned char buf[1];
+      I2C3_Start();
+      I2C3_Read(wAddr,buf,1,END_MODE_STOP);
+      return buf[0];
+}
+
+void main() {
+     //I2C3_Init();
+     I2C3_Init_Advanced(100000, &_GPIO_MODULE_I2C3_PA8_C9);
+     
+     while(1)
+     {
+      PCF_WrSingle(0x3C,0x00);
+      PCF_WrSingle(0x3D,0x00);
+      PCF_WrSingle(0x3E,0x00);
+      Delay_ms(1000);
+      PCF_WrSingle(0x3C,0xFF);
+      PCF_WrSingle(0x3D,0xFF);
+      PCF_WrSingle(0x3E,0xFF);
+      Delay_ms(1000);
+     }
+}
