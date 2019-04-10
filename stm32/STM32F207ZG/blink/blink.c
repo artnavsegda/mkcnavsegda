13ca050abd6146f1ab74d92e4988c557ee0e896a
Art Navsegda <artnavsegda@gmail.com>
2019-04-10 10:06:00 +0300

    wtf

--- stm32/STM32F207ZG/blink/blink.c
+++ stm32/STM32F207ZG/blink/blink.c
@@ -1,3 +1,13 @@
 void main() {
+  GPIO_Digital_Output(&GPIOC_BASE, _GPIO_PINMASK_ALL); // Set PORTC as digital output
 
+  //GPIOC_ODR = 0;
+
+  while(1) {
+    Delay_ms(5000);
+    //GPIOC_ODR = ~GPIOC_ODR; // Toggle PORTC
+    ODR15_GPIOC_ODR_bit = 0;
+    Delay_ms(5000);
+    ODR15_GPIOC_ODR_bit = 1;
+  }
 }
