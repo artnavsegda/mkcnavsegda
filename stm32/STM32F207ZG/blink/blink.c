ddfbe3c5e2d55612e0438b84ef6fdf2863585d21
Art Navsegda <artnavsegda@gmail.com>
2019-04-18 17:20:50 +0300

    wtf why

--- stm32/STM32F207ZG/blink/blink.c
+++ stm32/STM32F207ZG/blink/blink.c
@@ -1,13 +1,13 @@
 void main() {
-  GPIO_Digital_Output(&GPIOC_BASE, _GPIO_PINMASK_ALL); // Set PORTC as digital output
+  GPIO_Digital_Output(&GPIOE_BASE, _GPIO_PINMASK_5); // Set PORTE pin 5 as digital output, GSM_PW_ON
 
   //GPIOC_ODR = 0;
 
   while(1) {
     Delay_ms(5000);
     //GPIOC_ODR = ~GPIOC_ODR; // Toggle PORTC
-    ODR15_GPIOC_ODR_bit = 0;
+    ODR5_GPIOE_ODR_bit = 0;
     Delay_ms(5000);
-    ODR15_GPIOC_ODR_bit = 1;
+    ODR5_GPIOE_ODR_bit = 1;
   }
 }
