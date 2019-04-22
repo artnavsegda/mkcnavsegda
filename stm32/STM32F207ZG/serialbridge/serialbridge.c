2466bb640d85b98d05c31672702d8cf951680645
Art Navsegda <artnavsegda@gmail.com>
2019-04-22 15:33:01 +0300

    serialbridge

--- stm32/STM32F207ZG/serialbridge/serialbridge.c
+++ stm32/STM32F207ZG/serialbridge/serialbridge.c
@@ -1,5 +1,5 @@
 void PrintHandler(char c) {
-     UART1_Write(c);
+     UART6_Write(c);
 }
 
 void Uart3_interrupt() iv IVT_INT_USART3 ics ICS_AUTO {
@@ -8,12 +8,26 @@ void Uart3_interrupt() iv IVT_INT_USART3 ics ICS_AUTO {
      {
       rxdata = UART3_Read();
       UART1_Write(rxdata);
-      //PrintOut(PrintHandler,"RX3 %X\r\n",rxdata);
+      PrintOut(PrintHandler,"RX3 %c\r\n",rxdata);
+     }
+}
+
+void Uart1_interrupt() iv IVT_INT_USART1 ics ICS_AUTO {
+     unsigned rxdata;
+     if (UART1_Data_Ready())
+     {
+      rxdata = UART1_Read();
+      UART3_Write(rxdata);
+      PrintOut(PrintHandler,"RX1 %c\r\n",rxdata);
      }
 }
 
 void main() {
-     UART1_Init(115200);//(stdio/aux3)
+     UART6_Init_Advanced(115200, _UART_8_BIT_DATA, _UART_NOPARITY, _UART_ONE_STOPBIT, &_GPIO_MODULE_USART6_PC67);
+
+     UART1_Init(9600);//(stdio/aux3)
+     USART1_CR1bits.RXNEIE = 1; // enable uart rx interrupt
+     NVIC_IntEnable(IVT_INT_USART1); // enable interrupt vector
 
      GPIO_Digital_Output(&GPIOE_BASE, _GPIO_PINMASK_5); // Set PORTE pin 5 as digital output, GSM_PW_ON
      GPIO_Digital_Output(&GPIOE_BASE, _GPIO_PINMASK_6); // Set PORTE pin 5 as digital output, GSM_PW_KEY
@@ -24,7 +38,6 @@ void main() {
      Delay_ms(1000);
      ODR6_GPIOE_ODR_bit = 1; // PWRKEY up
      
-     UART3_Init(9600);// GSM uart
      UART3_Init_Advanced(9600, _UART_8_BIT_DATA, _UART_NOPARITY, _UART_ONE_STOPBIT, &_GPIO_MODULE_USART3_PC10_PB11); // gsm, also should not work
      USART3_CR1bits.RXNEIE = 1; // enable uart rx interrupt
      NVIC_IntEnable(IVT_INT_USART3); // enable interrupt vector
