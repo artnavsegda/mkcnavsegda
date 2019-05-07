362ea1dd2ccc426496b15c3ec79323f052dcf778
Art Navsegda <artnavsegda@gmail.com>
2019-05-07 11:16:24 +0300

    reset

--- stm32/STM32F207ZG/pn5180a/pn5180a.c
+++ stm32/STM32F207ZG/pn5180a/pn5180a.c
@@ -2,6 +2,11 @@ void PrintHandler(char c) {
      UART1_Write(c);
 }
 
+void PN5180A_Reset(void)
+{
+
+}
+
 void main() {
      I2C3_Init_Advanced(100000, &_GPIO_MODULE_I2C3_PA8_C9); // i2c start
      SPI1_Init_Advanced(_SPI_FPCLK_DIV64, _SPI_MASTER | _SPI_8_BIT | _SPI_CLK_IDLE_LOW | _SPI_FIRST_CLK_EDGE_TRANSITION | _SPI_MSB_FIRST | _SPI_SS_DISABLE | _SPI_SSM_ENABLE | _SPI_SSI_1, &_GPIO_MODULE_SPI1_PA56_PB5);
