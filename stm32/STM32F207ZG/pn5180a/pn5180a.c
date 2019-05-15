b980e6a7dda57e0b29ea9032032866009fe54539
Art Navsegda <artnavsegda@gmail.com>
2019-05-15 14:17:16 +0300

    rabotaet vrode

--- stm32/STM32F207ZG/pn5180a/pn5180a.c
+++ stm32/STM32F207ZG/pn5180a/pn5180a.c
@@ -9,7 +9,7 @@
 #define PN5180_BUSY  0b01000000
 #define PN5180_RESET 0b10000000
 
-#define PN5180_NSS 0b10000000000
+#define PN5180_NSS _GPIO_PINMASK_11
 
 void PrintHandler(char c) {
      UART1_Write(c);
@@ -26,8 +26,8 @@ bool digitalRead(uint8_t mask)
 
 void digitalWrite(uint16_t mask, bool level)
 {
-     if (level)
-        GPIOF_ODR != mask;
+     if (!level)
+        GPIOF_ODR |= mask;
      else
         GPIOF_ODR &= ~(mask);
 }
@@ -348,7 +348,8 @@ void main() {
      I2C3_Init_Advanced(100000, &_GPIO_MODULE_I2C3_PA8_C9); // i2c start
      PCF_WrSingle(0x3F,0xFF);//write weak input pull up
      SPI1_Init_Advanced(_SPI_FPCLK_DIV64, _SPI_MASTER | _SPI_8_BIT | _SPI_CLK_IDLE_LOW | _SPI_FIRST_CLK_EDGE_TRANSITION | _SPI_MSB_FIRST | _SPI_SS_DISABLE | _SPI_SSM_ENABLE | _SPI_SSI_1, &_GPIO_MODULE_SPI1_PA56_PB5);
-     GPIO_Digital_Output(&GPIOF_BASE, _GPIO_PINMASK_10); // Set PORTF pin 10 as digital output, SPI_CS_NFC_NSS
+     GPIO_Digital_Output(&GPIOF_BASE, _GPIO_PINMASK_8 | _GPIO_PINMASK_9 | _GPIO_PINMASK_10 |_GPIO_PINMASK_11); // Set PORTF pin 8,9,10,11 as digital output, SPI_CS0, SPI_CS1, SPI_CS2, SPI_CS3
+     GPIOF_ODR |= _GPIO_PINMASK_11 | _GPIO_PINMASK_10 | _GPIO_PINMASK_8; // CS2 output LOW
      UART1_Init(9600); //(stdio/aux3)
      UART1_Write_Text("start nfc test\r\n");
      
